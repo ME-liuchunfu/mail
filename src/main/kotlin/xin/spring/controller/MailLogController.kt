b414cd61f3bdcef7bda9e8317c14423c78bbfec7
spring <429829320@qq.com>
2019-09-12 13:34:31 +0800

    发送记录

---
+++ src/main/kotlin/xin/spring/controller/MailLogController.kt
@@ -0,0 +1,25 @@
+package xin.spring.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import xin.spring.annotation.DocLog
+import xin.spring.result.R
+import xin.spring.service.MailLogService
+import org.springframework.beans.factory.annotation.Autowired
+
+@DocLog("查询邮件发送记录")
+@RestController
+@RequestMapping("/sys/mail/")
+class MailLogController {
+	
+	@DocLog("邮件数据服务")
+	@Autowired
+	lateinit var mailLogService: MailLogService
+	
+	@RequestMapping("list")
+	fun findMailList(): R{
+		var result = mailLogService.findAll();
+		return R.ok().put("data", result)
+	}
+	
+}
